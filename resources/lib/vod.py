d121ebdd2c284f3f03ee59ee15aa8d04112a2faf
Maven85 <janis.semper@googlemail.com>
2018-07-10 08:25:30 +0200

    fixed play vod

--- resources/lib/vod.py
+++ resources/lib/vod.py
@@ -14,6 +14,6 @@ def playAsset(asset_id, infolabels=None, art=None, parental_rating=0):
         manifest_url = asset_info['ms_media_url']
 
     if infolabels is None:
-        info_tag, asset_info = nav.getInfoLabel(asset_info.get('type', ''), asset_info)
+        infolabels, asset_info = nav.getInfoLabel(asset_info.get('type', ''), asset_info)
 
-    skyticket.play(manifest_url, package_code=asset_info['package_code'], parental_rating=parental_rating, info_tag=info_tag, apix_id=str(asset_info['event_id']))
+    skyticket.play(manifest_url, package_code=asset_info['package_code'], parental_rating=parental_rating, info_tag=infolabels, apix_id=str(asset_info['event_id']))
